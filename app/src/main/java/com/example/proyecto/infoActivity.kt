31d1ba0ac439c8f247a5d960ec2914dfadf92d7a
priscillagonzalez <fatimag811@gmail.com>
2022-03-10 23:34:50 -0600

    Update Interface

--- app/src/main/java/com/example/proyecto/infoActivity.kt
+++ app/src/main/java/com/example/proyecto/infoActivity.kt
@@ -1,23 +1,16 @@
 package com.example.proyecto
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class infoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.supportActionBar?.hide()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.informacion)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val adapter = CustomAdapter()
