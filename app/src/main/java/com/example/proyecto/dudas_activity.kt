5326993effc4c89cf06475d299d09a04e0ae62b2
estef <estefelvi07@gmail.com>
2022-03-11 00:18:29 -0600

    comit 2

--- app/src/main/java/com/example/proyecto/dudas_activity.kt
+++ app/src/main/java/com/example/proyecto/dudas_activity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class dudas_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dudas)
+        setContentView(R.layout.fragment_dudas)
     }
 }
